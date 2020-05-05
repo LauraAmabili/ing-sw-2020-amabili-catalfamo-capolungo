eebbda452b698c57aae3fde1a6d7f3e1613d92d8
walter-catalfamo <walter.catalfamo@mail.polimi.it>
2020-05-05 02:20:30 +0200

    Multidirectional channel

--- src/main/java/it/polimi/ingsw/Network/Client/Client.java
+++ src/main/java/it/polimi/ingsw/Network/Client/Client.java
@@ -9,59 +9,60 @@ import java.io.*;
 import java.lang.reflect.Type;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Client {
 
-    private static int port;
-    private static Gson gson = new Gson();
-    private static String file = "./src/main/java/it/polimi/ingsw/resources/serverConf.json";
-    private static Socket socket;
+    private int port;
+    private Gson gson = new Gson();
+    private String file = "./src/main/java/it/polimi/ingsw/resources/serverConf.json";
+    private Socket socket;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
 
 
-    // get the input stream from the connected socket
-    private InputStream is;
-
-    // create a DataInputStream so we can read data from it.
-    private ObjectInputStream ois;
-
-    // get the output stream from the socket.
-    private OutputStream os;
-
-    // create an object output stream from the output stream so we can send an object through it
-    private ObjectOutputStream oos;
-
-    public Client() {
+    public Client() throws IOException {
         read();
-        try {
-            socket = new Socket("localhost", port);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            os = socket.getOutputStream();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            oos = new ObjectOutputStream(os);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Connected!");
+
     }
 
 
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Client client = new Client();
+        client.startClient();
+    }
 
-    public static void read() {
+    public void startClient() throws IOException, ClassNotFoundException {
+        socket = new Socket("localhost", port);
+        out = new ObjectOutputStream(socket.getOutputStream());
+        in = new ObjectInputStream(socket.getInputStream());
+
+        send("MessageFromClient");
+
+        System.out.println(receive());
+        socket.close();
+    }
+
+    public void send(String x) throws IOException {
+        out.writeObject(x);
+    }
+
+    public String receive() throws IOException, ClassNotFoundException {
+        String x = (String) in.readObject();
+        return x;
+    }
+
+    public void read() {
         FileReader fileReader = null;
         try {
             fileReader = new FileReader(file);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        Type userListType = new TypeToken<Integer>(){}.getType();
+        Type userListType = new TypeToken<Integer>() {
+        }.getType();
         assert fileReader != null;
         port = gson.fromJson(fileReader, userListType);
         try {
@@ -70,67 +71,5 @@ public class Client {
             e.printStackTrace();
         }
     }
-
-    public static List<Message> receiveListFromServer(){
-
-        // read the list of messages from the socket
-        List<Message> listOfMessages = null;
-        try {
-
-            listOfMessages = (List<Message>) ois.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return listOfMessages;
-    }
-
-    public Message receiveMessageFromServer(){
-        // read the message from the socket
-        Message message= null;
-        try {
-
-            message = (Message) ois.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return message;
-    }
-
-
-    public static void sendToServer(List<Message> listOfMessages){
-        try {
-            oos.writeObject(listOfMessages);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    public void sendToServer(Message message){
-        try {
-            oos.writeObject(message);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        new Client();
-        List<Message> greetings = new ArrayList<>();
-        greetings.add(new Greetings("Walter", "1", 0));
-        greetings.add(new Greetings("Walter", "2", 0));
-        greetings.add(new Greetings("Walter", "3", 0));
-        greetings.add(new Greetings("Walter", "4", 0));
-        sendToServer(greetings);
-
-        List<Message> listOfMessages = receiveListFromServer();
-
-
-
-
-        System.out.println("Closing socket and terminating program.");
-        socket.close();
-    }
-
 
 }
--- src/main/java/it/polimi/ingsw/Network/Server/ServerThread.java
+++ src/main/java/it/polimi/ingsw/Network/Server/ServerThread.java
@@ -1,119 +1,57 @@
 package it.polimi.ingsw.Network.Server;
 
+import it.polimi.ingsw.Network.Client.Client;
 import it.polimi.ingsw.Network.Message.Greetings;
-import it.polimi.ingsw.Network.Message.Message;
+import it.polimi.ingsw.Network2.Message;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.ArrayList;
+import java.net.UnknownHostException;
 import java.util.List;
 
-class ServerThread extends Thread implements Runnable{
+class ServerThread extends Thread implements Runnable {
 
-    Socket s=null;
+    Socket socket;
 
-    // get the input stream from the connected socket
-    InputStream is;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
 
-    // create a DataInputStream so we can read data from it.
-    ObjectInputStream ois;
-
-    // get the output stream from the socket.
-    OutputStream os;
-
-    // create an object output stream from the output stream so we can send an object through it
-    ObjectOutputStream oos;
+    public ServerThread(Socket socket) {
+        this.socket = socket;
 
 
-
-    public ServerThread(Socket s){
-        this.s=s;
-
-        try {
-            is = s.getInputStream();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            ois = new ObjectInputStream(is);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            os = s.getOutputStream();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            oos = new ObjectOutputStream(os);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
-
-
-
-
-
-    public List<Message> receiveListFromClient(){
-        // read the list of messages from the socket
-        List<Message> listOfMessages = null;
-        try {
-
-            listOfMessages = (List<Message>) ois.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return listOfMessages;
-    }
-
-    public Message receiveMessageFromClient(){
-        // read the message from the socket
-        Message message= null;
-        try {
-
-            message = (Message) ois.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return message;
-    }
-
-
-
-    public void sendToClient(List<Message> listOfMessages){
-        try {
-            oos.writeObject(listOfMessages);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    public void sendToClient(Message message){
-        try {
-            oos.writeObject(message);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
 
     public void run() {
+        ServerThread serverThread = new ServerThread(socket);
+        try {
+            serverThread.startServer();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
-        List<Message> listOfMessages = receiveListFromClient();
-        System.out.println(listOfMessages);
 
-        List<Message> greetings = new ArrayList<>();
-        greetings.add(new Greetings("Walter", "1", 0));
-        greetings.add(new Greetings("Walter", "2", 0));
-        greetings.add(new Greetings("Walter", "3", 0));
-        greetings.add(new Greetings("Walter", "4", 0));
-        sendToClient(greetings);
-
-        System.out.println("Closing sockets.");
     }
 
+    public void startServer() throws IOException, ClassNotFoundException {
+        out = new ObjectOutputStream(socket.getOutputStream());
+        in = new ObjectInputStream(socket.getInputStream());
+
+        System.out.println(receive());
+
+        send("MessageFromServer");
+
+
+        socket.close();
+    }
+
+    public void send(String x) throws IOException {
+        out.writeObject(x);
+    }
+
+    public String receive() throws IOException, ClassNotFoundException {
+        String x = (String) in.readObject();
+        return x;
+    }
 
 }
