5ac39d41b6d5af88d33ee03453ec5c822c86b7ad
RCapolungo <10569104@polimi.it>
2020-06-27 21:38:38 +0200

    Gui

--- src/main/java/it/polimi/ingsw/View/GUI/BoardController.java
+++ src/main/java/it/polimi/ingsw/View/GUI/BoardController.java
@@ -13,7 +13,6 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 public abstract class BoardController extends NotifyMessages {
