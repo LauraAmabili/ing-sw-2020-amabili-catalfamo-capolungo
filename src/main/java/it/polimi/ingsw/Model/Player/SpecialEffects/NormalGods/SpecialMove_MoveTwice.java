75cf70a5455b9203f9f11e6d80f0c1525d4cd73a
walter-catalfamo <walter.catalfamo@mail.polimi.it>
2020-07-01 13:34:45 +0200

    gui scenes

--- src/main/java/it/polimi/ingsw/Model/Player/SpecialEffects/NormalGods/SpecialMove_MoveTwice.java
+++ src/main/java/it/polimi/ingsw/Model/Player/SpecialEffects/NormalGods/SpecialMove_MoveTwice.java
@@ -12,6 +12,19 @@ import java.util.List;
 public class SpecialMove_MoveTwice extends PlayerDecorator {
 
     private final boolean hasSpecialMove;
+    private final boolean hasTwoInputMove;
+
+    private boolean enableSpecialMove;
+
+    @Override
+    public boolean isHasTwoInputMove() {
+        return hasTwoInputMove;
+    }
+
+    @Override
+    public boolean isHasSpecialMove() {
+        return hasSpecialMove;
+    }
 
     @Override
     public boolean isEnableSpecialMove() {
@@ -23,20 +36,15 @@ public class SpecialMove_MoveTwice extends PlayerDecorator {
         this.enableSpecialMove = enableSpecialMove;
     }
 
-    @Override
-    public boolean isHasSpecialMove() {
-        return hasSpecialMove;
-    }
-
-    private boolean enableSpecialMove;
 
     public SpecialMove_MoveTwice(PlayerInterface p) {
         super(p);
         hasSpecialMove = true;
+        hasTwoInputMove = true;
     }
 
     /**
-     * Standard Move
+     * Double move
      *
      * @param row
      * @param col
@@ -44,47 +52,17 @@ public class SpecialMove_MoveTwice extends PlayerDecorator {
      * @return
      */
     @Override
-    public synchronized boolean move(int row, int col, @NotNull Worker worker) {
-        if (availableCellsToMove(worker).contains(this.getBoard().getGrid()[row][col])) {
-            worker.getCurCell().setWorker(null);
-            worker.setOldCell(worker.getCurCell());
-            worker.setCurCell(this.getBoard().getGrid()[row][col]);
-            worker.getCurCell().setWorker(worker);
-            return true;
+    public synchronized boolean move(int rowBuild, int colBuild, int row, int col, @NotNull Worker worker) {
+        if (enableSpecialMove){
+            player.move(rowBuild, colBuild, worker);
+            return player.move(row, col, worker);
         }
-        return false;
+        else {
+            return false;
+        }
     }
 
 
-    /**
-     * returns the BoardCells such that:
-     * - distance(worker)<=2
-     * - there are not Workers
-     * - there are not Domes
-     * - if (moveUp), remove BoardCells such that BoardCell.level+1>Worker.level
-     * - if !(moveUp), remove BoardCells such that BoardCell.level>Worker.level
-     * @param worker Worker used
-     * @return ArrayList with the BoardCells
-     */
-    @Override
-    public List<BoardCell> availableCellsToMove(@NotNull Worker worker) {
-        if(enableSpecialMove) {
-            List<BoardCell> adj = this.getBoard().adjacentCells(worker.getCurCell());
-            for (BoardCell x : this.getBoard().adjacentCells(worker.getCurCell())) {
-                adj.addAll(this.getBoard().adjacentCells(x));
-            }
-            adj.removeIf((n) -> n.getWorker() != null);
-            adj.removeIf(BoardCell::getDome);
-            if (worker.getPlayerWorker().isMoveUp()) {
-                adj.removeIf((n) -> (n.getLevel() > worker.getCurCell().getLevel() + 1));
-            } else {
-                adj.removeIf((n) -> (n.getLevel() > worker.getCurCell().getLevel()));
-            }
-            return adj;
-        }
-        return player.availableCellsToMove(worker, false);
-
-    }
 
 
 }
